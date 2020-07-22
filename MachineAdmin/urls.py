6a08f48afe0d6d513e11324a05c07861db787750
jichengxi <948788684@qq.com>
2020-07-22 08:14:23 +0800

    8

--- MachineAdmin/urls.py
+++ MachineAdmin/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('physical/', include('jphysical.urls')),
+    path('virtual/', include('jvirtual.urls')),
+    path('container/', include('jcontainer.urls'))
 ]
